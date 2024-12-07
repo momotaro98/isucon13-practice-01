653fe28fd0a9440557f000f9117a18da84f30892
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2024-12-07 15:28:04 +0900

    mask init.sh init mysql because it's too slow to initialize

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -13,60 +13,60 @@ ISUCON_DB_USER=${ISUCON13_MYSQL_DIALCONFIG_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON13_MYSQL_DIALCONFIG_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON13_MYSQL_DIALCONFIG_DATABASE:-isupipe}
 
-# MySQLを初期化
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < init.sql
+# # MySQLを初期化
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < init.sql
 
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < initial_users.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < initial_users.sql
 
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < initial_livestreams.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < initial_livestreams.sql
 
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < initial_tags.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < initial_tags.sql
 
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < initial_livestream_tags.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < initial_livestream_tags.sql
 
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < initial_reservation_slots.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < initial_reservation_slots.sql
 
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < initial_reactions.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < initial_reactions.sql
 
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < initial_ngwords.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < initial_ngwords.sql
 
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < initial_livecomments.sql
+# mysql -u"$ISUCON_DB_USER" \
+# 		-p"$ISUCON_DB_PASSWORD" \
+# 		--host "$ISUCON_DB_HOST" \
+# 		--port "$ISUCON_DB_PORT" \
+# 		"$ISUCON_DB_NAME" < initial_livecomments.sql
 
 bash ../pdns/init_zone.sh 
 
